e5f12ab7a0d14967ec2b609f6e3d31f5faa4f412
piyas1234 <mypiyastv@gmail.com>
2021-05-29 13:36:09 +0600

    all

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,36 @@
-import React from "react";
-import "./Navbar.css";
+import React from 'react';
+import { NavLink } from 'react-router-dom'
+import HomeIcon from '@material-ui/icons/Home';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import BookmarksIcon from '@material-ui/icons/Bookmarks';
+import ScheduleIcon from '@material-ui/icons/Schedule';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import './Nav.css'
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <>
+      <div className="h-25 mt-5 d-None">
+        <img width="50px" src="https://icons.iconarchive.com/icons/iconarchive/red-orb-alphabet/256/Letter-G-icon.png" alt="" />
+      </div>
+      <div className="NavBody shadow">
+        <div className="h-25 mt-5">
+          <img width="50px" src="https://icons.iconarchive.com/icons/iconarchive/red-orb-alphabet/256/Letter-G-icon.png" alt="" />
+        </div>
+        <div className="h-50">
+          <div className="mt-3"><NavLink className="text-dark" to="/"><HomeIcon fontSize="large"></HomeIcon></NavLink></div>
+          <div className="mt-3" ><NavLink className="text-dark" to="/"><ShoppingCartIcon fontSize="large"></ShoppingCartIcon></NavLink></div>
+          <div className="mt-3"><NavLink className="text-dark" to="/"><BookmarksIcon fontSize="large"></BookmarksIcon></NavLink></div>
+          <div className="mt-3"><NavLink className="text-dark" to="/"><ScheduleIcon fontSize="large"></ScheduleIcon></NavLink></div>
+        </div>
+
+        <div className="h-25">
+          <div className="mt-3"><NavLink to="/"><AccountCircleIcon fontSize="large"></AccountCircleIcon></NavLink></div>
+        </div>
+
+      </div>
+    </>
   );
-};
+}
 
 export default Navbar;
